57d418d8f4c3669f847c4401eebbd216965e84b4
lorenzofal <63105821+lorenzofal@users.noreply.github.com>
2021-04-15 16:11:37 +0300

    detail.ejs creada con rutas y controladores

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,5 +2,13 @@ const autos = require('../data/autos')
 module.exports = {
     index: (req, res)=>{
         return res.render('index', {title: 'Pagina de Inicio', autos: autos.lista })
+    }, 
+    detail: (req, res)=>{
+        let id = req.params.id;
+        for(let i = 0; i < autos.length; i++){
+            if(autos[i].id == id){
+                return res.render('detail', {auto : autos[i]});
+            }
+        }
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 const homeController = require('../controllers/homeController');
 /* GET home page. */
 router.get('/', homeController.index);
+router.get('/detail/:id', homeController.detail);
 
 /*router.get('/', function(req, res, next) {
   res.render('index', { title: 'Productos' });
