b1fd0153a2e403b9f5b993396c5773beeaeba775
Felipedearamburu <fdearamburu@udesa.edu.ar>
2021-04-21 17:31:01 -0300

    Revert "commit error"
    This reverts commit 0d8aa94b6454f24fa91626bc305abb6c0356d8d2.

--- data/autos.js
+++ data/autos.js
@@ -3,13 +3,13 @@ let autos = {
         {
             id : 0,
             nombre : "Kia Rio",
-            precio : "1.600.000",
+            precio : "$1.600.000",
             imagen : "kiario.jpeg"
         },
         {
             id : 1,
             nombre : "Bmw X5",
-            precio : "4.000.000",
+            precio : "$4.000.000",
             imagen : "bmw.jpeg"
         },
         {
@@ -21,79 +21,79 @@ let autos = {
         {
             id : 3,
             nombre : "Volkswagen Golf",
-            precio : "2.000.000",
+            precio : "$2.000.000",
             imagen : "golf.jpeg"
         },
         {
             id : 4,
             nombre : "Mercedes Benz Clase C",
-            precio : "3.200.000",
+            precio : "$3.200.000",
             imagen : "mercedes.jpeg"
         },
         {
             id : 5,
             nombre : "Toyota Yaris",
-            precio : "1.400.000",
+            precio : "$1.400.000",
             imagen : "yarys.jpeg"
         },
         {
             id : 6,
             nombre : "Volkswagen Polo",
-            precio : "1.700.000",
+            precio : "$1.700.000",
             imagen : "golf.jpeg"
         },
         {
             id : 7,
             nombre : "Toyota Corolla",
-            precio : "1.600.000",
+            precio : "$1.600.000",
             imagen : "yarys.jpeg"
         },
         {
             id : 8,
             nombre : "Volkswagen Nivus",
-            precio : "2.000.000",
+            precio : "$2.000.000",
             imagen : "nivus.jpeg"
         },
         {
             id : 9,
             nombre : "Volkswagen T-Cross",
-            precio : "1.900.000",
+            precio : "$1.900.000",
             imagen : "t-cross.jpeg"
         },
         {
             id : 10,
             nombre : "Volkswagen Passat",
-            precio : "2.000.000",
+            precio : "$2.000.000",
             imagen : "passat.jpeg"
         },
         {
             id : 11,
             nombre : "Volkswagen Amarok",
-            precio : "2.700.000",
+            precio : "$2.700.000",
             imagen : "amarok.jpeg"
         },
         {
             id : 12,
             nombre : "Volkswagen Tiguan",
-            precio : "4.000.000",
+            precio : "$4.000.000",
             imagen : "tiguan.jpeg"
         },
         {
             id : 13,
             nombre : "Volkswagen Touareg",
-            precio : "2.300.000",
+            precio : "$2.300.000",
             imagen : "touareg.jpeg"
         },
         {
             id : 14,
             nombre : "Volkswagen Vento",
-            precio : "1.600.000",
+            precio : "$1.600.000",
             imagen : "vento.jpeg"
         },
         {
             id : 15,
             nombre : "Kia Sportage",
-            precio : "1.500.000",
+            precio : "$1.500.000",
             imagen : "sportage.jpeg"
         },
     ]
