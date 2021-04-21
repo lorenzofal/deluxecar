8f5a42d64fdd95c8f0984504bfa26169f5c5dfd4
Felipedearamburu <fdearamburu@udesa.edu.ar>
2021-04-21 04:45:40 -0300

    agregue mas productos

--- data/autos.js
+++ data/autos.js
@@ -1,54 +1,101 @@
 let autos = {
     lista : [
         {
-            id : 1,
+            id : 0,
             nombre : "Kia Rio",
             precio : "500.000",
             imagen : "kiario.jpeg"
         },
         {
-            id : 2,
+            id : 1,
             nombre : "Bmw X5",
             precio : "1500.000",
             imagen : "bmw.jpeg"
         },
         {
-            id : 3,
+            id : 2,
             nombre : "Toyota Etios",
             precio : "300.000",
             imagen : "etios.jpeg"
         },
         {
-            id : 4,
+            id : 3,
             nombre : "Volswagen Golf",
             precio : "900.000",
             imagen : "golf.jpeg"
         },
         {
-            id : 5,
+            id : 4,
             nombre : "Mercedes Benz Clase C",
             precio : "900.000",
             imagen : "mercedes.jpeg"
         },
         {
-            id : 6,
+            id : 5,
             nombre : "Toyota Yaris",
             precio : "900.000",
             imagen : "yarys.jpeg"
         },
         {
-            id : 7,
+            id : 6,
             nombre : "Volswagen Polo",
             precio : "900.000",
             imagen : "golf.jpeg"
         },
         {
-            id : 8,
+            id : 7,
             nombre : "Toyota Corolla",
             precio : "900.000",
             imagen : "yarys.jpeg"
-        }
-
+        },
+        {
+            id : 8,
+            nombre : "Volswagen Nivus",
+            precio : "900.00",
+            imagen : "nivus.jpeg"
+        },
+        {
+            id : 9,
+            nombre : "Volswagen T-Cross",
+            precio : "900.000",
+            imagen : "t-cross.jpeg"
+        },
+        {
+            id : 10,
+            nombre : "Volswagen Passat",
+            precio : "900.000",
+            imagen : "passat.jpeg"
+        },
+        {
+            id : 11,
+            nombre : "Volswagen Amarok",
+            precio : "900.000",
+            imagen : "amarok.jpeg"
+        },
+        {
+            id : 12,
+            nombre : "Volswagen Tiguan",
+            precio : "900.000",
+            imagen : "tiguan.jpeg"
+        },
+        {
+            id : 13,
+            nombre : "Volswagen Touareg",
+            precio : "900.000",
+            imagen : "touareg.jpeg"
+        },
+        {
+            id : 14,
+            nombre : "Volswagen Vento",
+            precio : "900.000",
+            imagen : "vento.jpeg"
+        },
+        {
+            id : 15,
+            nombre : "Kia Sportage",
+            precio : "900.000",
+            imagen : "sportage.jpeg"
+        },
     ]
 
 }
