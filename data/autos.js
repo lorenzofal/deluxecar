4ed8b1787f2851b1d2297999a4c3ab2c92e159a1
Felipedearamburu <fdearamburu@udesa.edu.ar>
2021-04-23 05:33:36 -0300

    detalle casi liquidada

--- data/autos.js
+++ data/autos.js
@@ -4,97 +4,113 @@ let autos = {
             id : 0,
             nombre : "Kia Rio",
             precio : "1.600.000",
-            imagen : "kiario.jpeg"
+            imagen : "kiario.jpeg",
+            descripcion : 'Hola como estas'
         },
         {
             id : 1,
             nombre : "Bmw X5",
             precio : "4.000.000",
-            imagen : "bmw.jpeg"
+            imagen : "bmw.jpeg",
+            descripcion : 'Hola como estas'
         },
         {
             id : 2,
             nombre : "Toyota Etios",
             precio : "1.100.000",
-            imagen : "etios.jpeg"
+            imagen : "etios.jpeg",
+            descripcion : 'Hola como estas'
         },
         {
             id : 3,
             nombre : "Volkswagen Golf",
             precio : "2.000.000",
-            imagen : "golf.jpeg"
+            imagen : "golf.jpeg",
+            descripcion : 'Que picado estoy perrooooo'
         },
         {
             id : 4,
             nombre : "Mercedes Benz Clase C",
             precio : "3.200.000",
-            imagen : "mercedes.jpeg"
+            imagen : "mercedes.jpeg",
+            descripcion : 'Hola como estas'
         },
         {
             id : 5,
             nombre : "Toyota Yaris",
             precio : "1.400.000",
-            imagen : "yarys.jpeg"
+            imagen : "yarys.jpeg",
+            descripcion : 'Hola como estas'
         },
         {
             id : 6,
             nombre : "Volkswagen Polo",
             precio : "1.700.000",
-            imagen : "golf.jpeg"
+            imagen : "golf.jpeg",
+            descripcion : 'Que picado estoy perroo'
         },
         {
             id : 7,
             nombre : "Toyota Corolla",
             precio : "1.600.000",
-            imagen : "yarys.jpeg"
+            imagen : "yarys.jpeg",
+            descripcion : 'Hola como estas'
         },
         {
             id : 8,
             nombre : "Volkswagen Nivus",
             precio : "2.000.000",
-            imagen : "nivus.jpeg"
+            imagen : "nivus.jpeg",
+            descripcion : 'Hola como estas'
         },
         {
             id : 9,
             nombre : "Volkswagen T-Cross",
             precio : "1.900.000",
-            imagen : "t-cross.jpeg"
+            imagen : "t-cross.jpeg",
+            descripcion : 'Hola como estas'
         },
         {
             id : 10,
             nombre : "Volkswagen Passat",
             precio : "2.000.000",
-            imagen : "passat.jpeg"
+            imagen : "passat.jpeg",
+            descripcion : 'Hola como estas'
         },
         {
             id : 11,
             nombre : "Volkswagen Amarok",
             precio : "2.700.000",
-            imagen : "amarok.jpeg"
+            imagen : "amarok.jpeg",
+            descripcion : 'Hola como estas'
         },
         {
             id : 12,
             nombre : "Volkswagen Tiguan",
             precio : "4.000.000",
-            imagen : "tiguan.jpeg"
+            imagen : "tiguan.jpeg",
+            descripcion : 'Hola como estas'
         },
         {
             id : 13,
             nombre : "Volkswagen Touareg",
             precio : "2.300.000",
-            imagen : "touareg.jpeg"
+            imagen : "touareg.jpeg", 
+            descripcion : 'Hola como estas'
         },
         {
             id : 14,
             nombre : "Volkswagen Vento",
             precio : "1.600.000",
-            imagen : "vento.jpeg"
+            imagen : "vento.jpeg",
+            descripcion : 'Hola como estas'
         },
         {
             id : 15,
             nombre : "Kia Sportage",
             precio : "1.500.000",
-            imagen : "sportage.jpeg"
+            imagen : "sportage.jpeg", 
+            descripcion : 'Hola como estas'
         },
     ]
 
