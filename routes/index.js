4392a88d5e409a2e4088b757062fe167ddc27a35
Felipedearamburu <fdearamburu@udesa.edu.ar>
2021-04-24 01:26:29 -0300

    reemplazo de variable en el entry point

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 const homeController = require('../controllers/homeController');
 
 /* GET home page. */
 router.get('/', homeController.index);
 router.get('/detail/:id', homeController.detail);
+
 
 
 /*router.get('/', function(req, res, next) {
