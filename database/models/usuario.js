da0df6acf985b4cee1c1a4780e033d793c9a1f9e
Diogenes Blaquier <diogenesblaquier@MacBook-Air-de-Diogenes.local>
2021-06-10 10:53:29 -0400

    cookies + model usuario

---
+++ database/models/usuario.js
@@ -0,0 +1,52 @@
+const { sequelize } = require(".");
+
+module.exports = (sequelize, dataTypes ) => {
+
+    let alias = 'Usuario';
+    let cols = {
+        id:{
+            autoIncrement: true,
+            primaryKey: true,
+            unsigned: true,
+            type: dataTypes.INTEGER
+
+
+        },
+
+        nombre:{
+            type: dataTypes.STRING,
+
+        },
+        Email : {
+            type: dataTypes.STRING,
+        },
+        Password: {
+            type: dataTypes.STRING,
+        },
+        Telefono:{
+            type: dataTypes.STRING,
+        },
+        
+        
+
+
+        
+
+
+
+
+    },
+
+
+
+
+};
+
+let config = {
+    tableName: "Usuarios",
+    timestamps: false,
+    underscored: true
+    
+}
+const Usuario = sequelize.define(alias,cols,config)
+return Usuario;
