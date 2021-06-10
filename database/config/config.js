54152268debde2dc50ca0ee9ca2fa4dd92121263
Diogenes Blaquier <diogenesblaquier@MacBook-Air-de-Diogenes.local>
2021-06-10 13:50:00 -0400

    Update config.js

--- database/config/config.js
+++ database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": "root",
-    "database": "deluxecars",
+    "database": "Proyectointegrador",
     "host": "127.0.0.1",
     "dialect": "mysql",
   },
